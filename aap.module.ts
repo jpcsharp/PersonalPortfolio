45259c0d6aed4022a2503c152f62da97d286a2e1
jpcsharp <127258766+jpcsharp@users.noreply.github.com>
2024-01-24 20:46:18 -0500

    Add files via upload

---
+++ aap.module.ts
@@ -0,0 +1,5 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import{AppRoutingModule} from './app-routing.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
